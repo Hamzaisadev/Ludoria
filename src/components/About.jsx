e1441e3aa6e8ea9c24b64bab345116fd05ee8291
Hamzaisadev <ishaqhamza212@gmail.com>
2024-12-29 20:51:09 +0500

    Update About component: enhance structure and add welcome message

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const About = () => {
-  return <div>Gu</div>;
+  return (
+    <div className="" id="about">
+      <div className="relative mb-8 mt-36 flex flex-col">
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          Welcome To Arcane h
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default About;
